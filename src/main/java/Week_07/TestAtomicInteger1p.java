3a3d080fa41da0ae9317fb85fdfce2f16fea79ff
CaiYun017 <tancaiyun123@gmail.com>
2025-05-16 12:40:34 +0800

    Add Real-TimeExercise files

--- src/main/java/Week_07/TestAtomicInteger1p.java
+++ src/main/java/Week_07/TestAtomicInteger1p.java
@@ -1,4 +1,41 @@
 package Week_07;
 
+import java.lang.reflect.ParameterizedType;
+
 public class TestAtomicInteger1p {
+    public static void main (String[] args) throws  InterruptedException {
+        CountProblem pt = new CountProblem();
+        Thread t1 = new Thread(pt,"t1");
+        Thread t2 = new Thread(pt,"t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count=" + pt.getCount());
+    }
+}
+
+class CountProblem implements Runnable {
+    private int count;
+
+    @Override
+    public void run() {
+        for (int i = 1; i<=5; i++) {
+            processSomething(i);
+            synchronized (this) { //New modification - synchronized block
+                count++;          // New modification - increment within synchronized block
+            }
+        }
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+    private void processSomething(int i) {
+        try {
+            Thread.sleep(i*200);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Week_07/ThreadComparison.java
+++ src/main/java/Week_07/ThreadComparison.java
@@ -1,4 +1,63 @@
 package Week_07;
 
 public class ThreadComparison {
+    private static final int NUM_THREADS = 10;
+    private static final int ITERATIONS = 100000;
+    private static int normalCounter = 0;
+    private static int syncCounter = 0;
+    private static final Object lock = new Object();
+
+    public static void main (String[] args) throws InterruptedException {
+        //Test normal threads
+        long normalStart = System.nanoTime();
+        NormalThread[] normalThreads = new NormalThread[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            normalThreads[i] = new NormalThread();
+            normalThreads[i].start();
+        }
+
+        for (NormalThread thread : normalThreads) {
+            thread.join();
+        }
+        long normalEnd = System.nanoTime();
+        double normalTime = (normalEnd - normalStart) / 1_000_000_000.0;
+
+        // Test synchronized threads
+        long syncStart = System.nanoTime();
+        SynchronizedThread[] syncThreads = new SynchronizedThread[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            syncThreads[i] = new SynchronizedThread();
+            syncThreads[i].start();
+        }
+        for (SynchronizedThread thread : syncThreads) {
+            thread.join();
+        }
+        long syncEnd = System.nanoTime();
+        double syncTime = (syncEnd - syncStart) / 1_000_000_000.0;
+
+        // Display results
+        System.out.println("Normal thread = " + String.format("%.8f", normalTime) + " seconds");
+        System.out.println("Synchronized thread = " + String.format("%.8f", syncTime) + " seconds");
+    }
+
+    static class NormalThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < ITERATIONS; i++) {
+                normalCounter++;
+            }
+        }
+    }
+
+    static class SynchronizedThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < ITERATIONS; i++) {
+                synchronized (lock) {
+                    syncCounter++;
+                }
+            }
+        }
+
+    }
 }
